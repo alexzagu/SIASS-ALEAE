73a5c5167e8ed1237b62c6ea88d2778eeb45db69
Alan Valdez <agvaldezc@Alans-MacBook-Pro.local>
2017-09-21 14:25:50 -0500

    DI1.9.1 Cambio de vista de varias a unificada en la vista de user.home con el contenido de student, así como hacer la tabla responsiva para vistas móviles.

--- SIASS/resources/views/pages/user/home.blade.php
+++ SIASS/resources/views/pages/user/home.blade.php
@@ -59,6 +59,65 @@
                 </div>
             @endif
 
+            @if($user->isStudent())
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 ">
+
+                            <h2>Informacion del Alumno</h2>
+                            <div class="table-responsive">
+                                <table style="width:100%" class="table">
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Correo Electronico</th>
+                                        <th>Carrera</th>
+                                        <th>Plan de Estudios</th>
+                                        <th>Campus</th>
+                                        <th>Estatus</th>
+                                        <th>Unidades Acreditadas</th>
+                                    </tr>
+                                    <tr>
+                                        <th>{{$user->name}}</th>
+                                        <th>{{$user->email}}</th>
+                                        <th>{{$student->major}}</th>
+                                        <th>{{$student->studyPlan}}</th>
+                                        <th>{{$student->campus}}</th>
+                                        <th>{{$student->studentStatus}}</th>
+                                        <th>{{$student->certifiedUnits}}</th>
+                                    </tr>
+                                </table>
+
+                                <h2>Servicio Social Ciudadano</h2>
+
+                                <table style="width:100%" class="table">
+                                    <tr>
+                                        <th>Total horas certificadas</th>
+                                        <th>Total horas registradas</th>
+                                    </tr>
+                                    <tr>
+                                        <th>{{$student->totalCertifiedHoursSSC}}</th>
+                                        <th>{{$student->totalRegisteredHoursSSC}}</th>
+                                    </tr>
+                                </table>
+
+                                <h2>Servicio Social Profesional</h2>
+
+                                <table style="width:100%" class="table">
+                                    <tr>
+                                        <th>Total horas certificadas</th>
+                                        <th>Total horas registradas</th>
+                                    </tr>
+                                    <tr>
+                                        <th>{{$student->totalCertifiedHoursSSP}}</th>
+                                        <th>{{$student->totalRegisteredHoursSSP}}</th>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
             @if (session('register-success'))
                 <div class="alert alert-success">
                     {{ session('register-success') }}
