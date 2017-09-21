1dc6afdea5ab465b518b04a0615dc08ca35f01a1
Evert Salinas González <evertsalinas88@gmail.com>
2017-09-21 13:20:28 -0500

    DI1.8.2 Se muestra el historial en la view del alumno

--- SIASS/resources/views/pages/student/home.blade.php
+++ SIASS/resources/views/pages/student/home.blade.php
@@ -7,10 +7,59 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Estudiante</h1>
-            <p>Tu perfil de usuario es de tipo estudiante</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ">
+
+                <h2>Informacion del Alumno</h2>
+
+                <table style="width:100%" class="table">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Correo Electronico</th>
+                        <th>Carrera</th>
+                        <th>Plan de Estudios</th>
+                        <th>Campus</th>
+                        <th>Estatus</th>
+                        <th>Unidades Acreditadas</th>
+                    </tr>
+                    <tr>
+                        <th>{{$user->name}}</th>
+                        <th>{{$user->email}}</th>
+                        <th>{{$student->major}}</th>
+                        <th>{{$student->studyPlan}}</th>
+                        <th>{{$student->campus}}</th>
+                        <th>{{$student->studentStatus}}</th>
+                        <th>{{$student->certifiedUnits}}</th>
+                    </tr>
+                </table>
+
+                <h2>Servicio Social Ciudadano</h2>
+
+                <table style="width:100%" class="table">
+                    <tr>
+                        <th>Total horas certificadas</th>
+                        <th>Total horas registradas</th>
+                    </tr>
+                    <tr>
+                        <th>{{$student->totalCertifiedHoursSSC}}</th>
+                        <th>{{$student->totalRegisteredHoursSSC}}</th>
+                    </tr>
+                </table>
+
+                <h2>Servicio Social Profesional</h2>
+
+                <table style="width:100%" class="table">
+                    <tr>
+                        <th>Total horas certificadas</th>
+                        <th>Total horas registradas</th>
+                    </tr>
+                    <tr>
+                        <th>{{$student->totalCertifiedHoursSSP}}</th>
+                        <th>{{$student->totalRegisteredHoursSSP}}</th>
+                    </tr>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
