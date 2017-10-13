8ab13b2d1997c69b227f53c5280bbbb72f19dbf7
Alan Valdez <agvaldezc@Alans-MacBook-Pro.local>
2017-10-12 20:02:14 -0500

    F1.10.5 Se creó la funcionalidad de confirmación de información en la forma de creación de un nuevo proyecto social, tanto para administradores como para socios formadores. También se incluye la funcionalidad de al querer editar la información, se regresa a la forma con toda la información actual ya llena.

--- SIASS/routes/web.php
+++ SIASS/routes/web.php
@@ -31,14 +31,19 @@ Route::get('/user', 'UserController@index');
 
 //Admin routes
 Route::get('admin/home', 'AdministratorController@index');
+
 Route::get('/admin/register-partner', 'PartnerController@create');
 Route::post('/admin/register-partner', 'PartnerController@store')->name('register-partner');
+
 Route::get('admin/register-social-service', 'SocialServiceController@create');
 Route::post('admin/register-social-service', 'SocialServiceController@store')->name('register-social-service');
+
+Route::post('admin/confirm-social-service', 'SocialServiceController@confirm')->name('confirm-social-service');
 //------------
 
 //Partner routes
 Route::get('partner/home', 'PartnerController@index');
+
 Route::get('partner/register-social-service', 'SocialServiceController@create');
 Route::post('partner/register-social-service', 'SocialServiceController@store')->name('register-social-service');
 //------------
