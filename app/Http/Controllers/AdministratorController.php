c0f9754660ee388101fcf338f5f0ac34bb423c6d
Alejandro Zamudio Guajardo <alex.zamudio88@gmail.com>
2017-11-18 01:39:14 -0600

    BUGFIX 1.23.21 Syntax error while including library.

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -7,7 +7,7 @@ use App\Student;
 use App\StudentService;
 use App\Partner;
 use Illuminate\Http\Request;
-use Carbon\Carbon
+use Carbon\Carbon;
 
 class AdministratorController extends Controller
 {
